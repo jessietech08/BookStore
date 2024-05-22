f35a6923b2bf99f5dfb75c34c412be2089beb80c
Jessie <jessietajalle08@gmail.com>
2024-05-22 10:57:44 -0700

    Removed unneeded test code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 class Book {
 }
-let myBook = new Book();
-myBook.isbn = "123";
-myBook.price = 9.99;
-myBook.title = "Programming for beginners";
-myBook.releaseDate = new Date(2024, 3, 22);
-console.log(myBook);
 window.onload = function () {
     let addBookBtn = document.querySelector("#add-book");
     addBookBtn.onclick = processBook;
--- ts/main.ts
+++ ts/main.ts
@@ -21,15 +21,6 @@ class Book {
     releaseDate: Date;
 }
 
-// book object test code
-let myBook = new Book();
-myBook.isbn = "123";
-myBook.price = 9.99;
-myBook.title = "Programming for beginners";
-myBook.releaseDate = new Date(2024, 3, 22); // months start at index 0
-
-console.log(myBook);
-
 window.onload = function() {
     // set up button click for add book form
     // # find element with id of add-book
